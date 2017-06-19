bb02f464989f8f536fb3f7d03f08eadb3b712d22
Bianca Colon Rosado <bcolon@taskrabbit.com>
2017-06-19 14:48:56 -0700

    Pins Users and Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :pins
+
+# Add secure passwords  
 #  validates :password, :presence => true,
- #                    :on => :create,
-  #                   :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+#            :on => :create,
+#            :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 
 end
